a1397bcdf99cd5f288b4acbd81141308c5dd2b42
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-11-24 03:27:17 +0900

    TRViS向けJSON出力機能を実装

--- TRViS.LocalServers.BveTs/TimetableServerPlugin.cs
+++ TRViS.LocalServers.BveTs/TimetableServerPlugin.cs
@@ -4,11 +4,16 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
 using AtsEx.PluginHost.Plugins;
 using AtsEx.PluginHost.Plugins.Extensions;
+
+using BveTypes.ClassWrappers;
+
+using TRViS.JsonModels;
 
 namespace TRViS.LocalServers.BveTs;
 
@@ -136,7 +141,109 @@ public partial class TimetableServerPlugin : PluginBase, IExtension
 			return GenResponse("404 Not Found", "text/plain", "Not Found", isHead);
 		}
 
-		return GenResponse("200 OK", TIMETABLE_FILE_MIME, "Hello, World! from AtsEX Extension", isHead);
+		if (!BveHacker.IsScenarioCreated)
+		{
+			return GenResponse("204 No Content", "text/plain", "No Content (Scenario Not Loaded)", isHead);
+		}
+
+		byte[] content;
+		try
+		{
+			content = GenerateJson();
+		}
+		catch (Exception ex)
+		{
+			return GenResponse("500 Internal Server Error", "text/plain", $"Internal Server Error: {ex.Message}\r\n{ex.StackTrace}", isHead);
+		}
+
+		return GenResponse("200 OK", TIMETABLE_FILE_MIME, GenerateJson(), isHead);
+	}
+
+	byte[] GenerateJson()
+	{
+		Scenario scenario = BveHacker.Scenario;
+		ScenarioInfo scenarioInfo = BveHacker.ScenarioInfo;
+		StationList stations = scenario.Route.Stations;
+		TimeTable timeTable = scenario.TimeTable;
+
+		TimetableRowData[] trvisTimetableRows = new TimetableRowData[stations.Count];
+		for (int i = 0; i < trvisTimetableRows.Length; i++)
+		{
+			int indexOfTimetableInstance = i + 1;
+			bool IsLastStop = i == (trvisTimetableRows.Length - 1);
+
+			trvisTimetableRows[i] = new TimetableRowData(
+				StationName: timeTable.NameTexts[indexOfTimetableInstance],
+				Location_m: stations[i].Location,
+				Longitude_deg: null,
+				Latitude_deg: null,
+				OnStationDetectRadius_m: null,
+				FullName: timeTable.NameTexts[indexOfTimetableInstance],
+				RecordType: null,
+				TrackName: null,
+				DriveTime_MM: null,
+				DriveTime_SS: null,
+				IsOperationOnlyStop: false,
+				IsPass: false,
+				HasBracket: false,
+				IsLastStop: IsLastStop,
+				Arrive: timeTable.ArrivalTimeTexts[indexOfTimetableInstance],
+				Departure: timeTable.DepertureTimeTexts[indexOfTimetableInstance],
+				RunInLimit: null,
+				RunOutLimit: null,
+				Remarks: null,
+				MarkerColor: null,
+				MarkerText: null,
+				WorkType: null
+			);
+		}
+
+		double motorCarCount = scenario.Vehicle.Dynamics.MotorCar.Count;
+		double trailerCarCount = scenario.Vehicle.Dynamics.TrailerCar.Count;
+		string motorCarCountStr = 0 < motorCarCount ? $"{motorCarCount:#.#}M" : string.Empty;
+		string trailerCarCountStr = 0 < trailerCarCount ? $"{trailerCarCount:#.#}T" : string.Empty;
+		TrainData trvisTrainData = new(
+			TrainNumber: scenarioInfo.Title,
+			MaxSpeed: null,
+			SpeedType: null,
+			NominalTractiveCapacity: $"{scenarioInfo.VehicleTitle}\n{motorCarCountStr}{trailerCarCountStr}",
+			CarCount: (int)Math.Ceiling(motorCarCount + trailerCarCount),
+			Destination: null,
+			BeginRemarks: null,
+			AfterRemarks: null,
+			Remarks: "Generated with TRViS Local Servers (AtsEX Extension)",
+			BeforeDeparture: null,
+			TrainInfo: null,
+			Direction: 1,
+			WorkType: null,
+			AfterArrive: null,
+			BeforeDeparture_OnStationTrackCol: null,
+			AfterArrive_OnStationTrackCol: null,
+			DayCount: 0,
+			IsRideOnMoving: false,
+			Color: null,
+			TimetableRows: trvisTimetableRows
+		);
+
+		WorkData trvisWorkData = new(
+			Name: scenarioInfo.RouteTitle,
+			AffectDate: null,
+			AffixContentType: null,
+			AffixContent: null,
+			Remarks: null,
+			HasETrainTimetable: false,
+			ETrainTimetableContentType: null,
+			ETrainTimetableContent: null,
+			Trains: [trvisTrainData]
+		);
+
+		WorkGroupData trvisWorkGroupData = new(
+			Name: "TRViS Local Servers (AtsEX Extension)",
+			DBVersion: 1,
+			Works: [trvisWorkData]
+		);
+
+		return JsonSerializer.SerializeToUtf8Bytes<WorkGroupData[]>([trvisWorkGroupData], new JsonSerializerOptions { WriteIndented = false });
 	}
 
 	private static (byte[], byte[]) GenResponse(string status, string contentType, string content, bool isHead = false)
